d75dd781c43961efc16a3c744fe7ffff2d9e8ddc
Dragos Tudorache <hedphug@gmail.com>
2015-04-21 01:25:14 +0300

    add todos

--- src/client/components/presentator/presentator.js
+++ src/client/components/presentator/presentator.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import classNames from 'classnames';
-import {RouteHandler, Link} from 'react-router';
-import Reflux from 'reflux';
+import React from 'react'
+import classNames from 'classnames'
+import {RouteHandler, Link} from 'react-router'
+import Reflux from 'reflux'
 
-import PageControls from './page-controls';
-import SlideNavigation from '../navigation/slide-navigation';
-import PresentatorStore from '../../stores/presentator/store';
-import PresentatorActions from '../../stores/presentator/actions';
+import PageControls from './page-controls'
+import SlideNavigation from '../navigation/slide-navigation'
+import PresentatorStore from '../../stores/presentator/store'
+import PresentatorActions from '../../stores/presentator/actions'
 
-const noop = function() {};
+const noop = function() {}
 
 /*
   TODO: #refactor responsabilites
@@ -25,11 +25,17 @@ const noop = function() {};
   The delegates are react components or just plain js objects.
 
 
-  TODO: #refactor routehandler props events
+  TODO: #move slideNavigation to the presentator store
 
-  The `onSlidePresented` and `onSlideRemoved` are not clear and
-  their intention is not well understood.
-  Rename the handlers to `onSliderActivated`, `onSliderDeactivated`
+  Instead of having `shouldPresentNextSlide` and `shouldPresentPrevSlide`
+  inside the component, move them to the store and update the component
+  through the state changes
+
+
+  TODO: #build a navigation input component
+
+  This component could be used to wrap other components(in this case,
+  the Presentator component) that need to react at navigation inputs.
 
  */
 const Presentator = React.createClass({
@@ -51,97 +57,100 @@ const Presentator = React.createClass({
   },
 
   componentWillMount() {
-    this.slideNavigation = new SlideNavigation();
-    this.slideNavigation.onNextSlide = this.shouldPresentNextSlide;
-    this.slideNavigation.onPrevSlide = this.shouldPresentPrevSlide;
+    this.slideNavigation = new SlideNavigation()
+    this.slideNavigation.onNextSlide = this.shouldPresentNextSlide
+    this.slideNavigation.onPrevSlide = this.shouldPresentPrevSlide
   },
 
   componentWillUnmount() {
     // is there really a need for nullifying the callbacks?
-    this.slideNavigation.onNextSlide = null;
-    this.slideNavigation.onPrevSlide = null;
-    this.slideNavigation = null;
+    this.slideNavigation.onNextSlide = null
+    this.slideNavigation.onPrevSlide = null
+    this.slideNavigation = null
   },
 
-  // TODO: refactor @#2
+  // TODO: refactor @2
   onSliderMounted() {
-    const presentation = this.currentPresentation();
-    PresentatorActions.presentationStarted(presentation);
-    this.slideNavigation.activate();
+    const presentation = this.currentPresentation()
+    PresentatorActions.presentationStarted(presentation)
+    // TODO: refactor @3
+    this.slideNavigation.activate()
     // TODO: should pass the slide that has been presented??
-    this.props.onDidPresent({index: 'hardcoded 1'});
+    this.props.onDidPresent({index: 'hardcoded 1'})
   },
 
-  // TODO: refactor @#2
+  // TODO: refactor @2
   onSliderUnmounted() {
-    PresentatorActions.presentationEnded();
-    this.slideNavigation.deactivate();
+    PresentatorActions.presentationEnded()
+    // TODO: refactor @3
+    this.slideNavigation.deactivate()
     // TODO: should pass the slide that has been presented??
-    this.props.onWillHide({index: 'hardcoded 1'});
+    this.props.onWillHide({index: 'hardcoded 1'})
   },
 
-  // TODO: refactor @#2
+  // TODO: refactor @3
   shouldPresentNextSlide() {
-    const {id, slide, endIndex, startIndex} = this.currentPresentation();
-    const nextIndex = slide + 1;
-    if (nextIndex > endIndex) return;
-    this.gotoSlide({index: nextIndex});
+    const {id, slide, endIndex, startIndex} = this.currentPresentation()
+    const nextIndex = slide + 1
+    if (nextIndex > endIndex) return
+    this.gotoSlide({index: nextIndex})
   },
 
-  // TODO: refactor @#2
+  // TODO: refactor @3
   shouldPresentPrevSlide() {
-    const {id, slide, endIndex, startIndex} = this.currentPresentation();
-    const prevIndex = slide - 1;
-    if (prevIndex < startIndex) return;
-    this.gotoSlide({index: prevIndex});
+    const {id, slide, endIndex, startIndex} = this.currentPresentation()
+    const prevIndex = slide - 1
+    if (prevIndex < startIndex) return
+    this.gotoSlide({index: prevIndex})
   },
 
-  // TODO: refactor @#2
+  // TODO: refactor @2
   gotoSlide(index) {
     PresentatorActions.toNextSlide({
       id, slide: index, startIndex, endIndex
-    });
+    })
     this.context.router.transitionTo('slide-presentation',
-      {id, index});
+      {id, index})
   },
 
   // TODO: refactor to use `this.gotoSlide()` for navigation
   /*shouldPresentPrevSlide() {
-    const {id, slide, endIndex, startIndex} = this.currentPresentation();
-    const prevIndex = slide - 1;
-    if (prevIndex < startIndex) return;
+    const {id, slide, endIndex, startIndex} = this.currentPresentation()
+    const prevIndex = slide - 1
+    if (prevIndex < startIndex) return
     PresentatorActions.toPrevSlide({
       id, slide: prevIndex, startIndex, endIndex
-    });
+    })
     this.context.router.transitionTo('slide-presentation',
-      {id, index: prevIndex});
+      {id, index: prevIndex})
   },*/
 
   // TODO: rename handler
-  // TODO: refactor @#2
+  // TODO: refactor @2
   onCloseModal() {
-    this.props.onShouldHide();
+    this.props.onShouldHide()
   },
 
+  // TODO: move this to the presenter store
   currentPresentation() {
-    const {id, index} = this.context.router.getCurrentParams();
+    const {id, index} = this.context.router.getCurrentParams()
     return {
       id: parseInt(id),
       slide: parseInt(index),
       startIndex: this.props.startIndex,
       endIndex: this.props.endIndex
-    };
+    }
   },
 
   render() {
     const presentatorCls = classNames('w-100 h-100 brs bg-silver', {
       'dn': !this.state.active
-    });
+    })
 
     const controlsCls = {
       'leftActive': !this.state.atSlideStart,
       'rightActive': !this.state.atSlideEnd
-    };
+    }
 
     return (
       <div id="presentator" className={presentatorCls}>
@@ -163,6 +172,6 @@ const Presentator = React.createClass({
       </div>
     )
   }
-});
+})
 
-export default Presentator;
+export default Presentator
